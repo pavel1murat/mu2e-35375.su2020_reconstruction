1e859b59b1348f2f08b4537e945461b03b08006d
Pavel Murat <murat@fnal.gov>
2021-01-05 10:31:23 -0600

    fix plot dimensions

--- scripts/init_local_datasets.C
+++ scripts/init_local_datasets.C
@@ -39,6 +39,13 @@ void init_local_datasets(stn_book* Book) {
     hf = Book->NewHistFile("su2020.cele0s51b2" ,"","track_comp_use_mva_ffff_1070");
     hf = Book->NewHistFile("su2020.cele0s51b2" ,"","su2020_track_ana_dar_11_1070");
 
+    d  = Book->NewDataset ("su2020.cele0s61b1" ,"",-1,   1000000); 
+    hf = Book->NewHistFile("su2020.cele0s61b1" ,"","su2020_track_ana_11_1070");
+    hf = Book->NewHistFile("su2020.cele0s61b1" ,"","su2020_track_ana_12_1070");
+
+    d  = Book->NewDataset ("su2020.cele0s61b2" ,"",-1,   1000000); 
+    hf = Book->NewHistFile("su2020.cele0s61b2" ,"","su2020_track_ana_10_1070");
+
     d  = Book->NewDataset ("su2020.cosm0s71b0" ,"",-1,   1000000); // cluster included 
     hf = Book->NewHistFile("su2020.cosm0s71b0" ,"","su2020_cosmic_ana_dar"  );
     hf = Book->NewHistFile("su2020.cosm0s71b0" ,"","su2020_cosmic_ana_par"  );
